7969ba49c1e4130fc6925cca3f447d58b6a48eb0
Your Name <mis.monterio@hotmail.com>
2021-02-04 19:00:40 -0300

    commit inicial

---
+++ RaizQuadrada_While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TerceiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            Console.WriteLine("Digite um número: ");
+            numero = Math.Sqrt(double.Parse(Console.ReadLine()));
+
+            while (numero >= 0);
+            {               
+                Console.WriteLine(numero.ToString("F3"));
+                Console.WriteLine("Digite outro número: ");
+                numero = Math.Sqrt(double.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Número negativo");
+           
+
+        }
+    }
+}
