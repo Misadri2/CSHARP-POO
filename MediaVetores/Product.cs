0b6c4666a11c3c0cfd512e97ff4a608c85fdbab5
Your Name <mis.monterio@hotmail.com>
2021-02-22 10:33:21 -0300

    commit inicial

---
+++ MediaVetores/Product.cs
@@ -0,0 +1,8 @@
+namespace MediaVetores
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ MediaVetores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MediaVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Informe quantos produtos você quer cadastrar:");
+
+            int n = int.Parse(Console.ReadLine()); // Para informar número de produtos que serão inseridos
+
+            Product[] vect = new Product[n];    //Instanciando a classe e o vetor
+
+            for(int i = 0; i < n; i++){             // Utilizando "for" para repetir a operação n vezes
+                Console.WriteLine("Informe nome e preço do produto:");
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product{ Name=name, Price=price};  // Instanciando nome e preço da classe no vetor
+            }
+
+            double sum = 0;
+
+            for(int i = 0; i < n; i++){
+                sum = sum + vect[i].Price;  // Utilizando "for" para somar o número de vezes que possui o vetor e indicando o Price
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("Average Price = " + avg.ToString("F2"));
+        }
+    }
+}
